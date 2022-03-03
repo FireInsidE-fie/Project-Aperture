d3cec64b79d380125b94b6ff495d0394d24347b3
FireInsidE-fie <firefoxspinnie@protonmail.ch>
2022-03-03 14:33:24 +0100

    Embed the script directly in index

--- links.js
+++
@@ -1,6 +0,0 @@
-const linksElement = document.getElementById('links');
-const hamburgerMenu = document.querySelector('#menu-toggle');
-
-if (hamburgerMenu.checked) {
-  linksElement.hidden = false;
-}
